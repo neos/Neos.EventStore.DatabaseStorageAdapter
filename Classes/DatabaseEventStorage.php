8d70bfc06aa86b2a837b84b6fd0c879d28f34668
Dominique Feyer <dfeyer@ttree.ch>
2016-09-17 23:07:34 +0200

    TASK: Refactor the Store
    This change replace the two tables store with a single table store. The
    store is now aggregate agnostic and use only stream_name to replace
    aggregate_identifier and aggregate_name

--- Classes/DatabaseEventStorage.php
+++ Classes/DatabaseEventStorage.php
@@ -11,26 +11,25 @@ namespace Neos\EventStore\DatabaseStorageAdapter;
  * source code.
  */
 
-use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Doctrine\DBAL\Query\QueryBuilder;
 use Neos\Cqrs\Domain\Timestamp;
+use Neos\Cqrs\Event\EventInterface;
 use Neos\Cqrs\Event\EventTransport;
-use Neos\Cqrs\Event\EventType;
+use Neos\Cqrs\Message\MessageMetadata;
 use Neos\EventStore\DatabaseStorageAdapter\Factory\ConnectionFactory;
 use Neos\EventStore\DatabaseStorageAdapter\Persistence\Doctrine\DataTypes\DateTimeType;
-use Neos\EventStore\EventStream;
 use Neos\EventStore\EventStreamData;
-use Neos\EventStore\Exception\AggregateNotFoundException;
 use Neos\EventStore\Exception\StorageConcurrencyException;
 use Neos\EventStore\Storage\EventStorageInterface;
-use Neos\EventStore\Storage\PreviousEventsInterface;
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Flow\Property\PropertyMapper;
+use TYPO3\Flow\Property\PropertyMappingConfiguration;
+use TYPO3\Flow\Property\TypeConverter\ObjectConverter;
 
 /**
  * Database event storage, for testing purpose
  */
-class DatabaseEventStorage implements EventStorageInterface, PreviousEventsInterface
+class DatabaseEventStorage implements EventStorageInterface
 {
     /**
      * @var ConnectionFactory
@@ -50,236 +49,121 @@ class DatabaseEventStorage implements EventStorageInterface, PreviousEventsInter
     protected $runtimeCache = [];
 
     /**
-     * @var array
-     */
-    protected $runtimeVersionCache = [];
-
-    /**
-     * @param string $identifier
+     * @param string $streamName
      * @return EventStreamData
      */
-    public function load(string $identifier)
+    public function load(string $streamName)
     {
-        $version = $this->getCurrentVersion($identifier);
-        $cacheKey = md5($identifier . '.' . $version);
+        $version = $this->getCurrentVersion($streamName);
+        $cacheKey = md5($streamName . '.' . $version);
         if (isset($this->runtimeCache[$cacheKey])) {
             return $this->runtimeCache[$cacheKey];
         }
         $conn = $this->connectionFactory->get();
-        $commitName = $this->connectionFactory->getCommitName();
         $queryBuilder = $conn->createQueryBuilder();
         $query = $queryBuilder
-            ->select('data, aggregate_name')
-            ->from($commitName)
-            ->andWhere('aggregate_identifier_hash = :aggregate_identifier_hash')
-            ->orderBy('version', 'ASC')
-            ->setParameter('aggregate_identifier_hash', md5($identifier));
+            ->select('event, metadata')
+            ->from($this->connectionFactory->getStreamTableName())
+            ->andWhere('stream_name_hash = :stream_name_hash')
+            ->orderBy('commit_version', 'ASC')
+            ->addOrderBy('event_version', 'ASC')
+            ->setParameter('stream_name_hash', md5($streamName));
 
-        list($aggregateName, $data) = $this->eventStreamFromCommitQuery($query);
+        $data = $this->eventStreamFromCommitQuery($query);
 
-        if ($aggregateName === null) {
+        if ($data === []) {
             return null;
         }
 
-        $cacheKey = md5($identifier . '.' . $version);
-        $this->runtimeCache[$cacheKey] = new EventStreamData($identifier, $aggregateName, $data, $version);
+        $cacheKey = md5($streamName . '.' . $version);
+        $this->runtimeCache[$cacheKey] = new EventStreamData($data, $version);
 
         return $this->runtimeCache[$cacheKey];
     }
 
     /**
-     * @param string $streamIdentifier
-     * @param string $aggregateIdentifier
-     * @param string $aggregateName
+     * @param string $streamName
      * @param array $data
-     * @param integer $version
+     * @param int $commitVersion
+     * @return int
      * @throws StorageConcurrencyException
      */
-    public function commit(string $streamIdentifier, string $aggregateIdentifier, string $aggregateName, array $data, int $version)
+    public function commit(string $streamName, array $data, int $commitVersion)
     {
-        $stream = new EventStreamData($aggregateIdentifier, $aggregateName, $data, $version);
+        $stream = new EventStreamData($data, $commitVersion);
         $conn = $this->connectionFactory->get();
 
-        $commitName = $this->connectionFactory->getCommitName();
-
         $queryBuilder = $conn->createQueryBuilder();
-
-        $streamData = array_map(function (EventTransport $eventTransport) {
-            return $this->propertyMapper->convert($eventTransport, 'array');
-        }, $stream->getData());;
 
         $now = Timestamp::create();
 
-        $streamData = json_encode($streamData, JSON_PRETTY_PRINT);
         $query = $queryBuilder
-            ->insert($commitName)
+            ->insert($this->connectionFactory->getStreamTableName())
             ->values([
-                'identifier' => ':identifier',
-                'version' => ':version',
-                'data' => ':data',
-                'data_hash' => ':data_hash',
-                'created_at' => ':created_at',
-                'aggregate_identifier' => ':aggregate_identifier',
-                'aggregate_identifier_hash' => ':aggregate_identifier_hash',
-                'aggregate_name' => ':aggregate_name',
-                'aggregate_name_hash' => ':aggregate_name_hash'
+                'stream_name' => ':stream_name',
+                'stream_name_hash' => ':stream_name_hash',
+                'commit_version' => ':commit_version',
+                'event_version' => ':event_version',
+                'event' => ':event',
+                'metadata' => ':metadata',
+                'recorded_at' => ':recorded_at'
             ])
             ->setParameters([
-                'identifier' => $streamIdentifier,
-                'version' => $version,
-                'data' => $streamData,
-                'data_hash' => md5($streamData),
-                'created_at' => $now,
-                'aggregate_identifier' => $aggregateIdentifier,
-                'aggregate_identifier_hash' => md5($aggregateIdentifier),
-                'aggregate_name' => $aggregateName,
-                'aggregate_name_hash' => md5($aggregateName)
+                'stream_name' => $streamName,
+                'stream_name_hash' => md5($streamName),
+                'commit_version' => $commitVersion,
+                'recorded_at' => $now,
             ], [
-                'identifier' => \PDO::PARAM_STR,
+                'stream_name' => \PDO::PARAM_STR,
+                'stream_name_hash' => \PDO::PARAM_STR,
                 'version' => \PDO::PARAM_INT,
-                'data' => \PDO::PARAM_STR,
-                'data_hash' => \PDO::PARAM_STR,
-                'created_at' => DateTimeType::DATETIME_MICRO,
-                'aggregate_identifier' => \PDO::PARAM_STR,
-                'aggregate_identifier_hash' => \PDO::PARAM_STR,
-                'aggregate_name' => \PDO::PARAM_STR,
-                'aggregate_name_hash' => \PDO::PARAM_STR
+                'event' => \PDO::PARAM_STR,
+                'metadata' => \PDO::PARAM_STR,
+                'recorded_at' => DateTimeType::DATETIME_MICRO,
             ]);
 
-        try {
+        $version = 1;
+        array_map(function (EventTransport $eventTransport) use ($query, &$version) {
+            $event = $this->stringify($this->propertyMapper->convert($eventTransport->getEvent(), 'array'));
+            $metadata = $this->stringify($this->propertyMapper->convert($eventTransport->getMetaData(), 'array'));
+            $query->setParameter('event_version', $version);
+            $query->setParameter('event', $event);
+            $query->setParameter('metadata', $metadata);
             $query->execute();
-        } catch (UniqueConstraintViolationException $exception) {
-            throw new StorageConcurrencyException(
-                sprintf(
-                    'Aggregate root versions mismatch, storage exception, version %d',
-                    $version
-                ), [], 1472296099
-            );
-        }
+            $version++;
+        }, $stream->getData());
 
-        $this->commitStream($streamIdentifier, $version, $aggregateIdentifier, $aggregateName, $stream);
+        return $commitVersion;
     }
 
     /**
-     * @param string $commitIdentifier
-     * @param integer $commitVersion
-     * @param string $aggregateIdentifier
-     * @param string $aggregateName
-     * @param EventStreamData $streamData
-     */
-    protected function commitStream(string $commitIdentifier, int $commitVersion, string $aggregateIdentifier, string $aggregateName, EventStreamData $streamData)
-    {
-        $conn = $this->connectionFactory->get();
-        $queryBuilder = $conn->createQueryBuilder();
-        $streamName = $this->connectionFactory->getStreamName();
-        /** @var EventTransport $eventTransport */
-        foreach ($streamData->getData() as $version => $eventTransport) {
-            $event = $eventTransport->getEvent();
-            $properties = $this->propertyMapper->convert($eventTransport->getEvent(), 'array');
-            $properties = json_encode($properties, JSON_PRETTY_PRINT);
-            $timestamp = $eventTransport->getTimestamp();
-            $name = EventType::get($event);
-            $query = $queryBuilder
-                ->insert($streamName)
-                ->values([
-                    'identifier' => ':identifier',
-                    'commit_version' => ':commit_version',
-                    'version' => ':version',
-                    'type' => ':type',
-                    'type_hash' => ':type_hash',
-                    'properties' => ':properties',
-                    'created_at' => ':created_at',
-                    'aggregate_identifier' => ':aggregate_identifier',
-                    'aggregate_identifier_hash' => ':aggregate_identifier_hash',
-                    'aggregate_name' => ':aggregate_name',
-                    'aggregate_name_hash' => ':aggregate_name_hash'
-                ])
-                ->setParameters([
-                    'identifier' => $commitIdentifier,
-                    'commit_version' => $commitVersion,
-                    'version' => $version + 1,
-                    'type' => $name,
-                    'type_hash' => md5($name),
-                    'properties' => $properties,
-                    'created_at' => $timestamp,
-                    'aggregate_identifier' => $aggregateIdentifier,
-                    'aggregate_identifier_hash' => md5($aggregateIdentifier),
-                    'aggregate_name' => $aggregateName,
-                    'aggregate_name_hash' => md5($aggregateName)
-                ], [
-                    'identifier' => \PDO::PARAM_STR,
-                    'commit_version' => \PDO::PARAM_STR,
-                    'version' => \PDO::PARAM_INT,
-                    'type' => \PDO::PARAM_STR,
-                    'type_hash' => \PDO::PARAM_STR,
-                    'properties' => \PDO::PARAM_STR,
-                    'created_at' => DateTimeType::DATETIME_MICRO,
-                    'aggregate_identifier' => \PDO::PARAM_STR,
-                    'aggregate_identifier_hash' => \PDO::PARAM_STR,
-                    'aggregate_name' => \PDO::PARAM_STR,
-                    'aggregate_name_hash' => \PDO::PARAM_STR
-                ]);
-            $query->execute();
-        }
-    }
-
-    /**
-     * @param string $identifier
+     * @param string $streamName
      * @return boolean
      */
-    public function contains(string $identifier): bool
+    public function contains(string $streamName): bool
     {
-        return $this->getCurrentVersion($identifier) > 1 ? true : false;
+        return $this->getCurrentVersion($streamName) > 1 ? true : false;
     }
 
     /**
-     * @param  string $identifier
+     * @param  string $streamName
      * @return integer Current Aggregate Root version
      */
-    public function getCurrentVersion(string $identifier): int
+    public function getCurrentVersion(string $streamName): int
     {
         $conn = $this->connectionFactory->get();
-        $commitName = $this->connectionFactory->getCommitName();
         $queryBuilder = $conn->createQueryBuilder();
         $query = $queryBuilder
-            ->select('version')
-            ->from($commitName)
-            ->andWhere('aggregate_identifier = :aggregate_identifier')
-            ->orderBy('version', 'DESC')
+            ->select('commit_version')
+            ->from($this->connectionFactory->getStreamTableName())
+            ->andWhere('stream_name_hash = :stream_name_hash')
+            ->orderBy('commit_version', 'DESC')
+            ->addOrderBy('event_version', 'DESC')
             ->setMaxResults(1)
-            ->setParameter('aggregate_identifier', $identifier);
+            ->setParameter('stream_name_hash', md5($streamName));
 
         $version = (integer)$query->execute()->fetchColumn();
-        $this->runtimeVersionCache[$identifier] = $version;
         return $version ?: 0;
-    }
-
-    /**
-     * @param string $identifier
-     * @param integer $untilVersion
-     * @return EventStream
-     * @throws AggregateNotFoundException
-     */
-    public function getPreviousEvents(string $identifier, int $untilVersion): EventStream
-    {
-        $conn = $this->connectionFactory->get();
-        $commitName = $this->connectionFactory->getCommitName();
-        $queryBuilder = $conn->createQueryBuilder();
-        $query = $queryBuilder
-            ->select('data, aggregate_name')
-            ->from($commitName)
-            ->andWhere('aggregate_identifier = :aggregate_identifier AND version >= :untilVersion')
-            ->orderBy('version', 'DESC')
-            ->setParameter('aggregate_identifier', $identifier)
-            ->setParameter('untilVersion', $untilVersion);
-
-        list($aggregateName, $data) = $this->eventStreamFromCommitQuery($query);
-
-        if ($aggregateName === null) {
-            throw new AggregateNotFoundException(sprintf('Aggregate with identifier %s, version=%d, not found', $identifier, $untilVersion), 1472306367);
-        }
-
-        return new EventStream($identifier, $aggregateName, $data, $untilVersion);
     }
 
     /**
@@ -288,17 +172,30 @@ class DatabaseEventStorage implements EventStorageInterface, PreviousEventsInter
      */
     protected function eventStreamFromCommitQuery(QueryBuilder $query): array
     {
-        $aggregateName = null;
+        $configuration = new PropertyMappingConfiguration();
+        $configuration->allowAllProperties();
+        $configuration->setTypeConverterOption(
+            ObjectConverter::class,
+            ObjectConverter::CONFIGURATION_OVERRIDE_TARGET_TYPE_ALLOWED,
+            true
+        );
+
         $data = [];
-        foreach ($query->execute()->fetchAll() as $commit) {
-            $aggregateName = $commit['aggregate_name'];
-            $data = array_merge($data, array_map(function (array $eventData) {
-                return $this->propertyMapper->convert($eventData, EventTransport::class);
-            }, json_decode($commit['data'], true)));
+        foreach ($query->execute()->fetchAll() as $stream) {
+            $data[] = new EventTransport(
+                $this->propertyMapper->convert(json_decode($stream['event'], true), EventInterface::class, $configuration),
+                $this->propertyMapper->convert(json_decode($stream['metadata'], true), MessageMetadata::class, $configuration)
+            );
         }
-        if ($aggregateName === null) {
-            return [null, $data];
-        }
-        return [$aggregateName, $data];
+        return $data;
+    }
+
+    /**
+     * @param array $data
+     * @return string
+     */
+    protected function stringify(array $data): string
+    {
+        return json_encode($data, JSON_PRETTY_PRINT | JSON_PRESERVE_ZERO_FRACTION);
     }
 }
--- Classes/Schema/EventStoreSchema.php
+++ Classes/Schema/EventStoreSchema.php
@@ -26,83 +26,28 @@ final class EventStoreSchema
      * @param Schema $schema
      * @param string $name
      */
-    public static function createCommit(Schema $schema, string $name)
-    {
-        $table = $schema->createTable($name);
-
-        // UUID4 of the commit
-        $table->addColumn('identifier', Type::STRING, ['fixed' => true, 'length' => 36]);
-
-        // Version of the aggregate after event was recorded
-        $table->addColumn('version', Type::BIGINT, ['unsigned' => true]);
-
-        // Events of the commit
-        $table->addColumn('data', Type::TEXT);
-        $table->addColumn('data_hash', Type::STRING, ['length' => 32]);
-
-        // Timestamp of the commit
-        $table->addColumn('created_at', DateTimeType::DATETIME_MICRO);
-
-        // UUID4 of linked aggregate
-        $table->addColumn('aggregate_identifier', Type::TEXT);
-        $table->addColumn('aggregate_identifier_hash', Type::STRING, ['length' => 32]);
-
-        // Class of the linked aggregate
-        $table->addColumn('aggregate_name', Type::STRING, ['length' => 1000]);
-        $table->addColumn('aggregate_name_hash', Type::STRING, ['length' => 32]);
-
-        $table->setPrimaryKey(['identifier']);
-
-        // Concurrency check on database level
-        $table->addUniqueIndex(['aggregate_identifier_hash', 'version'], $name . '_v_uix');
-
-        $table->addIndex(['data_hash'], $name . '_dh');
-        $table->addIndex(['aggregate_name_hash'], $name . '_anh');
-    }
-
-    /**
-     * Use this method when you work with a single stream strategy
-     *
-     * @param Schema $schema
-     * @param string $name
-     */
     public static function createStream(Schema $schema, string $name)
     {
         $table = $schema->createTable($name);
 
-        // UUID4 of the event
-        $table->addColumn('identifier', Type::STRING, ['fixed' => true, 'length' => 36]);
+        // UUID4 of the stream
+        $table->addColumn('stream_name', Type::TEXT);
+        $table->addColumn('stream_name_hash', Type::STRING, ['length' => 32]);
 
-        // Commit version
+        // Version of the aggregate after event was recorded
         $table->addColumn('commit_version', Type::BIGINT, ['unsigned' => true]);
 
-        // Version of the event
-        $table->addColumn('version', Type::BIGINT, ['unsigned' => true]);
+        // Version of the event in the current commit
+        $table->addColumn('event_version', Type::BIGINT, ['unsigned' => true]);
 
-        // Name of the event
-        $table->addColumn('type', Type::STRING, ['length' => 1000]);
-        $table->addColumn('type_hash', Type::STRING, ['length' => 32]);
+        // Events of the stream
+        $table->addColumn('event', Type::TEXT);
+        $table->addColumn('metadata', Type::TEXT);
 
-        // Event payload
-        $table->addColumn('properties', Type::TEXT);
+        // Timestamp of the stream
+        $table->addColumn('recorded_at', DateTimeType::DATETIME_MICRO);
 
-        // Timestamp of the event
-        $table->addColumn('created_at', DateTimeType::DATETIME_MICRO);
-
-        // UUID4 of linked aggregate
-        $table->addColumn('aggregate_identifier', Type::TEXT);
-        $table->addColumn('aggregate_identifier_hash', Type::STRING, ['length' => 32]);
-
-        // Class of the linked aggregate
-        $table->addColumn('aggregate_name', Type::STRING, ['length' => 1000]);
-        $table->addColumn('aggregate_name_hash', Type::STRING, ['length' => 32]);
-
-        // Concurrency check on database level
-        $table->setPrimaryKey(['identifier', 'version'], $name . '_v_uix');
-
-        $table->addIndex(['aggregate_identifier_hash', 'commit_version'], $name . '_ai_cv');
-
-        $table->addIndex(['aggregate_name_hash'], $name . '_anh');
+        $table->setPrimaryKey(['stream_name_hash', 'commit_version', 'event_version']);
     }
 
     /**
